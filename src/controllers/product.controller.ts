5cb15d76f99398ae3359ea78aa34b494780a2009
Khoironi Kurnia Syah <zekhoi.app@gmail.com>
2022-11-10 00:58:51 +0800

    static method

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,23 +1,28 @@
-import express, { Request, Response } from "express";
+import { Request, Response } from "express";
 
 class ProductController {
-  findAll = (req: Request, res: Response) => {
+  static findAll = (req: Request, res: Response) => {
     res.json({ message: "Get all products" });
   };
-  find = (req: Request, res: Response) => {
+
+  static find = (req: Request, res: Response) => {
     const productId = req.params.id;
     res.json({ message: `Get a product with id: ${productId}` });
   };
-  create = (req: Request, res: Response) => {
+
+  static create = (req: Request, res: Response) => {
     res.json({ message: "Create a product" });
   };
-  update = (req: Request, res: Response) => {
+
+  static update = (req: Request, res: Response) => {
     const productId = req.params.id;
     res.json({ message: `Update a product with id: ${productId}` });
   };
-  delete = (req: Request, res: Response) => {
+
+  static delete = (req: Request, res: Response) => {
     const productId = req.params.id;
     res.json({ message: `Delete a product with id: ${productId}` });
   };
 }
-export default new ProductController();
+
+export default ProductController;
